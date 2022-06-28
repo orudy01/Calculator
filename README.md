5eb0b357183d0762e60d1f31fb8ccf2e238b996b
orudy01 <orudy01@icloud.com>
2022-06-28 11:59:19 -0700

    Designed calculator and made math basic funtions.
    Need to begin the javascript side of things adding functionality to
    each button and displaying the proper answer on the calculator.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Calculator
+
+From Odin Project, "Our final project is going to combine everything you’ve learned so far: you’re going to make an on-screen calculator using JavaScript, HTML, and CSS."
---
+++ script.js
@@ -0,0 +1,20 @@
+
+function add(num1, num2){
+    return num1+num2;
+}
+
+function subtract(num1, num2){
+    return num1-num2;
+}
+
+function multiply(num1, num2){
+    return num1*num2;
+}
+
+function divide(num1, num2){
+    return num1/num2;
+}
+
+function operate(operate,num1,num2){
+    return operate(num1,num2);
+}
