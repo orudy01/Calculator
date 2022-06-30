651bb10c35c7f530ee535e6df572af8bb89d40a1
orudy01 <orudy01@icloud.com>
2022-06-30 12:28:17 -0700

    Add function works properly and shows on screen
    Still need to add functionality to the rest of the basic math
    funcions.

--- script.js
+++ script.js
@@ -1,20 +1,93 @@
+let calcDisplay = document.getElementById("calc-Display");
+let num1 = 0;
+let num2 = 0;
+let answer = 0;
+let operand = undefined;
+let operandClicked = false;
 
-function add(num1, num2){
-    return num1+num2;
+let currentDisplay = '0'
+
+calcDisplay.textContent = currentDisplay;
+
+function addToValue(num) {
+    if (operandClicked === true) {
+        changeDisplay(num);
+        num2 = parseInt(currentDisplay);
+        console.log("num2:")
+        console.log(num2);
+    }
+    else {
+        changeDisplay(num);
+        num1 = parseInt(currentDisplay);
+        console.log("num1:")
+        console.log(num1);
+    }
+
 }
 
-function subtract(num1, num2){
-    return num1-num2;
+function changeDisplay(button) {
+    if (currentDisplay === '0') {
+        currentDisplay = button;
+        calcDisplay.textContent = currentDisplay;
+    }
+    else {
+        currentDisplay = currentDisplay + button
+        calcDisplay.textContent = currentDisplay;
+    }
 }
 
-function multiply(num1, num2){
-    return num1*num2;
+function resetDisplay() {
+    currentDisplay = '0'
+    num1 = 0;
+    num2 = 0;
+    calcDisplay.textContent = currentDisplay;
 }
 
-function divide(num1, num2){
-    return num1/num2;
+function prepNextValue(operandChoice) {
+    if(operandClicked===true){
+        showAnswer();
+    }
+    operand = operandChoice;
+    operandClicked = true;
+    currentDisplay = '0';
+    
 }
 
-function operate(operate,num1,num2){
-    return operate(num1,num2);
+function showAnswer(){
+    operate(operand,num1,num2);
+    num1=answer;
+    operandClicked = false;
+
+    calcDisplay.textContent = answer;
+    
+    console.log('answer');
+    console.log(answer);
+}
+
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(op, num1, num2) {
+   switch(op){
+    case 'add':
+        answer =  add(num1,num2);
+        console.log("num1:")
+        console.log(num1);
+        console.log("num2:")
+        console.log(num2);
+   }
 }
